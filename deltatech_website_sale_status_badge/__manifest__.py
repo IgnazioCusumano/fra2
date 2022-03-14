a6d90a749200ae1cd1ffdc2120071b0d8e788775
ABC SRL <69294609+abcstrategie@users.noreply.github.com>
2022-03-14 12:59:46 +0100

    [ADD] app deltatech_website_sale_status_badge

---
+++ deltatech_website_sale_status_badge/__manifest__.py
@@ -0,0 +1,19 @@
+# ©  2015-2020 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+{
+    "name": "eCommerce Sale Order status - badge",
+    "category": "Website",
+    "summary": "eCommerce Sale Order status - badge",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Terrabit, Dorin Hongu",
+    "website": "https://www.terrabit.ro",
+    "depends": ["deltatech_website_sale_status"],
+    "data": ["views/sale_view.xml"],
+    "qweb": [],
+    "images": ["static/description/main_screenshot.png"],
+    "installable": True,
+    "development_status": "Mature",
+    "maintainers": ["dhongu"],
+}
